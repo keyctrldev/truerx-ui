0ba6cc8c28fb563ea47c8e903b959b0168958edb
Aniket Nimbark <devaniketnimbark@gmail.com>
2024-09-16 19:03:40 +0530

    - Setup ESLint and Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+/**
+ * customize Prettier settings
+ */
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSameLine: true,
+    bracketSpacing: true,
+    printWidth: 120,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: 'all',
+};
