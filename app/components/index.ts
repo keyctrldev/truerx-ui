4f139b7ca776dc417b0f5814a4702ac833f0b7b1
Aniket Nimbark <devaniketnimbark@gmail.com>
2024-09-18 18:20:52 +0530

    - Create Custom Toast Component

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,2 +1,3 @@
 export { default as AppText } from './app-text/AppText';
-export {default as ListViewCell} from './list-view-cell/ListViewCell';
+export { default as ListViewCell } from './list-view-cell/ListViewCell';
+export { default as CustomToast } from './custom-toast/CustomToast';
--- app/utils/GlobalFunctions.ts
+++ app/utils/GlobalFunctions.ts
@@ -1,4 +1,8 @@
 // Define common functions used throughout the application.
 
-export const tempFunction = () => {
+export const capitalizeFLetter = (text: string): string => {
+    if (typeof text !== 'string' || text.length === 0) {
+        return text;
+    }
+    return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
 };
