a7ecdb4659b07d4f842ec77e08f30acafcd62674
Aniket Nimbark <devaniketnimbark@gmail.com>
2024-09-20 19:10:19 +0530

    - Implement Toast component in LoginScreen

--- app/components/custom-toast/CustomToastStyle.ts
+++ app/components/custom-toast/CustomToastStyle.ts
@@ -29,10 +29,9 @@ export const styles = StyleSheet.create({
         color: Colors.black,
         textAlign: "left",
         fontWeight: '400',
-        letterSpacing: 0.5,
         marginLeft: horizontalScale(15),
         marginTop: verticalScale(3),
-        fontSize: moderateScale(15),
+        fontSize: moderateScale(14),
     },
     sideBar: {
         width: horizontalScale(5),
