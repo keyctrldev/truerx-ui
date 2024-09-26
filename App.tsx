a3df4fab0fa1b7ad417b606c08377d9e27e8413e
Aniket Nimbark <devaniketnimbark@gmail.com>
2024-09-26 17:23:58 +0530

    - Added Firebase messaging & notifee setup for push notification

--- App.tsx
+++ App.tsx
@@ -1,9 +1,25 @@
 import 'react-native-gesture-handler';
+import { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { RootStack } from './app/navigation';
 import { ToastProvider } from './app/context';
+import messaging from '@react-native-firebase/messaging';
+import { fetchAndStorePushTokenIfPossible, onNotifeeMessageReceived } from './app/utils';
 
 const App = (): React.JSX.Element => {
+  useEffect(() => {
+    fetchAndStorePushTokenIfPossible();
+    const unsubscribeOnMessage = messaging().onMessage(onNotifeeMessageReceived);
+
+    const unsubscribeOnNotificationOpenedApp = messaging().onNotificationOpenedApp(onNotifeeMessageReceived);
+
+    messaging().setBackgroundMessageHandler(onNotifeeMessageReceived);
+
+    return () => {
+      unsubscribeOnMessage();
+      unsubscribeOnNotificationOpenedApp();
+    };
+  }, []);
   return (
     <ToastProvider>
       <NavigationContainer>
--- app/utils/GlobalFunctions.ts
+++ app/utils/GlobalFunctions.ts
@@ -1,4 +1,7 @@
-// Define common functions used throughout the application.
+import messaging, { FirebaseMessagingTypes } from '@react-native-firebase/messaging';
+
+import notifee, { AndroidImportance, AndroidVisibility } from '@notifee/react-native';
+import { Alert } from 'react-native';
 
 export const capitalizeFLetter = (text: string): string => {
   if (typeof text !== 'string' || text.length === 0) {
@@ -17,5 +20,62 @@ export const getStatusColor = (status: string): string => {
       return '#8F3333';
     default:
       return '#000000';
+  }
+};
+
+export const fetchAndStorePushTokenIfPossible = async (): Promise<void> => {
+  try {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      await messaging().registerDeviceForRemoteMessages();
+      const token = await messaging().getToken();
+      console.info('FCM :', token);
+    }
+  } catch (error) {
+    Alert.alert('Error fetching FCM token');
+  }
+};
+
+export const onNotifeeMessageReceived = async (message: FirebaseMessagingTypes.RemoteMessage): Promise<void> => {
+  try {
+    if (message && message.notification) {
+      const { title, body } = message.notification;
+      const { messageId, data } = message;
+
+      const channelId = await notifee.createChannel({
+        id: 'notifications',
+        name: 'notifications',
+        vibration: true,
+        importance: AndroidImportance.HIGH,
+        visibility: AndroidVisibility.PUBLIC,
+      });
+
+      await notifee.displayNotification({
+        id: messageId,
+        title: title ?? 'No title',
+        body: body ?? 'No body',
+        data,
+        android: {
+          channelId,
+          pressAction: {
+            id: 'default',
+          },
+          vibrationPattern: [100, 300, 100, 300],
+          autoCancel: true,
+          badgeCount: 1,
+          importance: AndroidImportance.HIGH,
+        },
+        ios: {
+          badgeCount: 1,
+          sound: 'default',
+        },
+      });
+    }
+  } catch (error) {
+    Alert.alert('Error displaying notification:');
   }
 };
