d820649eeabbd09fd448b644092bf8f755a873ee
narsingoju shivani <narsingojushivani4@gmail.com>
2024-09-30 13:34:54 +0530

    Added pagination

--- app/modules/Claims/ClaimsScreen.tsx
+++ app/modules/Claims/ClaimsScreen.tsx
@@ -1,73 +1,44 @@
-import React from "react";
-import { FlatList, Image, TouchableOpacity, View } from "react-native";
-import {
-  ActivityLoader,
-  AppText,
-  ListViewCell,
-  SafeAreaContainer,
-} from "../../components";
-import { styles } from "./ClaimsScreenStyle";
-import { Icons } from "../../assets";
-import { claimsScreenStrings } from "../../constants";
-import useClaimsScreen from "./useClaimsScreen";
+import React from 'react';
+import { FlatList, Image, TouchableOpacity, View } from 'react-native';
+import { ActivityLoader, AppText, ListViewCell, SafeAreaContainer } from '../../components';
+import { styles } from './ClaimsScreenStyle';
+import { Icons } from '../../assets';
+import { claimsScreenStrings } from '../../constants';
+import useClaimsScreen from './useClaimsScreen';
 const ClaimsScreen = () => {
-  const { handleLogOutPress, isLoading, claimsData } = useClaimsScreen();
+  const { handleLogOutPress, isLoading, claimsData, loadMore } = useClaimsScreen();
   return (
     <SafeAreaContainer style={styles.claimContainerStyle}>
       {isLoading && <ActivityLoader isVisible={isLoading} />}
       <View style={styles.headerContainer}>
-        <AppText style={styles.title}>
-          {claimsScreenStrings.claimsDemoHeading}
-        </AppText>
+        <AppText style={styles.title}>{claimsScreenStrings.claimsDemoHeading}</AppText>
         <View style={styles.iconContainer}>
           <TouchableOpacity style={styles.iconWrapper} activeOpacity={0.8}>
-            <Image
-              source={Icons.bellIcon}
-              style={styles.icon}
-              resizeMode="contain"
-            />
+            <Image source={Icons.bellIcon} style={styles.icon} resizeMode="contain" />
           </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.iconWrapper}
-            activeOpacity={0.8}
-            onPress={handleLogOutPress}
-          >
-            <Image
-              source={Icons.profileIcon}
-              style={styles.icon}
-              resizeMode="contain"
-            />
+          <TouchableOpacity style={styles.iconWrapper} activeOpacity={0.8} onPress={handleLogOutPress}>
+            <Image source={Icons.profileIcon} style={styles.icon} resizeMode="contain" />
           </TouchableOpacity>
         </View>
       </View>
 
       {/* Claims History Section */}
       <View style={styles.claimsHistoryContainer}>
-        <AppText style={styles.claimsHistoryTitle}>
-          {claimsScreenStrings.historyHeading}
-        </AppText>
+        <AppText style={styles.claimsHistoryTitle}>{claimsScreenStrings.historyHeading}</AppText>
       </View>
 
       {/* Claims and Status Sections */}
       <View style={styles.claimsStatusRow}>
-        <AppText style={styles.columnHeader}>
-          {claimsScreenStrings.claimSection}
-        </AppText>
-        <AppText style={[styles.columnHeader, styles.statusText]}>
-          {claimsScreenStrings.statusSection}
-        </AppText>
+        <AppText style={styles.columnHeader}>{claimsScreenStrings.claimSection}</AppText>
+        <AppText style={[styles.columnHeader, styles.statusText]}>{claimsScreenStrings.statusSection}</AppText>
       </View>
       <FlatList
         data={claimsData}
         keyExtractor={(_, index) => `${index}claimItem`}
         renderItem={({ item, index }) => (
-          <ListViewCell
-            typeOfConsultance={item.name}
-            status={item.status}
-            Description={item.description}
-            key={index}
-          />
+          <ListViewCell typeOfConsultance={item.name} status={item.status} Description={item.description} key={index} />
         )}
+        onEndReached={loadMore}
       />
     </SafeAreaContainer>
   );
--- app/modules/Claims/useClaimsScreen.ts
+++ app/modules/Claims/useClaimsScreen.ts
@@ -12,7 +12,8 @@ const useClaimsScreen = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [claimsData, setClaimsData] = useState<ClaimsDataItem[]>([]);
   const { showToast } = useToast();
-  const page: number = 0;
+  const [hasMore, setHasMore] = useState<boolean>(true);
+  const [page, setPage] = useState<number>(0);
   const limit: number = 5;
 
   const handleLogOutPress = async (): Promise<void> => {
@@ -38,14 +39,22 @@ const useClaimsScreen = () => {
   };
 
   const getClaimsData = async () => {
+    if (!hasMore) return;
+
     try {
       setIsLoading(true);
       const accesssToken = await AsyncStorageService.loadAccessToken();
+
       if (accesssToken) {
         const parsedOuter = JSON.parse(accesssToken?.token);
         const token: string = parsedOuter.token;
         const response = await getAllClaims(token, page, limit);
-        setClaimsData(response);
+
+        if (response.length > 0) {
+          setClaimsData(previousData => [...previousData, ...response]);
+        } else {
+          setHasMore(false);
+        }
       }
     } catch (error: any) {
       showToast(error?.message, 'ERROR');
@@ -53,14 +62,18 @@ const useClaimsScreen = () => {
       setIsLoading(false);
     }
   };
+  const loadMore = () => {
+    setPage(page + 1);
+  };
 
   useEffect(() => {
     getClaimsData();
-  }, []);
+  }, [page]);
   return {
     handleLogOutPress,
     isLoading,
     claimsData,
+    loadMore,
   };
 };
 
