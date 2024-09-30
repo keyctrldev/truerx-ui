3136f0185bbabdb1d3c5a3bf9c748fc510f4a0ec
narsingoju shivani <narsingojushivani4@gmail.com>
2024-09-30 10:20:29 +0530

    Added pagination to the claims

--- app/api/ClaimsApis.ts
+++ app/api/ClaimsApis.ts
@@ -1,20 +1,25 @@
-import { ClaimsDataItem } from "../types";
-import { truerxInstance } from "./apiInstance";
+import { ClaimsDataItem, TokenData } from '../types';
+import { truerxInstance } from './apiInstance';
 
 export const getClaimsByUserId = async (userId: number): Promise<ClaimsDataItem> => {
-    try {
-        const response = await truerxInstance.get<ClaimsDataItem>(`/claims/${userId}`);
-        return response.data;
-    } catch (error: any) {
-        throw error.response ? error.response.data : error.message;
-    }
+  try {
+    const response = await truerxInstance.get<ClaimsDataItem>(`/claims/${userId}`);
+    return response.data;
+  } catch (error: any) {
+    throw error.response ? error.response.data : error.message;
+  }
 };
 
-export const getAllClaims = async (): Promise<ClaimsDataItem[]> => {
-    try {
-        const response = await truerxInstance.get<ClaimsDataItem[]>(`/claims`);
-        return response.data;
-    } catch (error: any) {
-        throw error.response ? error.response.data : error.message;
-    }
+export const getAllClaims = async (token: string, page: number, limit: number): Promise<ClaimsDataItem[]> => {
+  const offset: number = page * limit;
+  try {
+    const response = await truerxInstance.get<ClaimsDataItem[]>(`/claims?offset=${offset}&limit=${limit}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error: any) {
+    throw error.response ? error.response.data : error.message;
+  }
 };
--- app/modules/Claims/useClaimsScreen.ts
+++ app/modules/Claims/useClaimsScreen.ts
@@ -1,58 +1,67 @@
 import { useEffect, useState } from 'react';
 import { ParamListBase, useNavigation } from '@react-navigation/native';
-import AsyncStorageService from '../../utils/AsyncStorageService'
+import AsyncStorageService from '../../utils/AsyncStorageService';
 import { Routes } from '../../constants';
 import { getAllClaims } from '../../api/ClaimsApis';
 import { useToast } from '../../context';
-import { ClaimsDataItem } from '../../types';
+import { ClaimsDataItem, TokenData } from '../../types';
 import { StackNavigationProp } from '@react-navigation/stack';
 
 const useClaimsScreen = () => {
-    const navigation = useNavigation<StackNavigationProp<ParamListBase>>()
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [claimsData, setClaimsData] = useState<ClaimsDataItem[]>([]);
-    const { showToast } = useToast()
+  const navigation = useNavigation<StackNavigationProp<ParamListBase>>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [claimsData, setClaimsData] = useState<ClaimsDataItem[]>([]);
+  const { showToast } = useToast();
+  const page: number = 0;
+  const limit: number = 5;
 
-    const handleLogOutPress = async (): Promise<void> => {
-        try {
-            await AsyncStorageService.removeAccessToken()
-            await AsyncStorageService.storeBiometrics({
-                isBiometricsAuthEnabled: false,
-                isUserLoggedIn: false,
-                userName: ''
-            }).then(() => {
-                navigation.reset({
-                    index: 0,
-                    routes: [{
-                        name: Routes.login,
-                    }]
-                })
-            })
-        } catch (e) {
-            throw new Error('Failed to load biometrics from async storage.');
-        }
-    };
-
-    const getClaimsData = async () => {
-        try {
-            setIsLoading(true)
-            const response = await getAllClaims()
-            setClaimsData(response)
-        } catch (error: any) {
-            showToast(error?.message, 'ERROR');
-        } finally {
-            setIsLoading(false);
-        }
+  const handleLogOutPress = async (): Promise<void> => {
+    try {
+      await AsyncStorageService.removeAccessToken();
+      await AsyncStorageService.storeBiometrics({
+        isBiometricsAuthEnabled: false,
+        isUserLoggedIn: false,
+        userName: '',
+      }).then(() => {
+        navigation.reset({
+          index: 0,
+          routes: [
+            {
+              name: Routes.login,
+            },
+          ],
+        });
+      });
+    } catch (e) {
+      throw new Error('Failed to load biometrics from async storage.');
     }
+  };
 
-    useEffect(() => {
-        getClaimsData()
-    }, [])
-    return {
-        handleLogOutPress,
-        isLoading,
-        claimsData
+  const getClaimsData = async () => {
+    try {
+      setIsLoading(true);
+      const accesssToken = await AsyncStorageService.loadAccessToken();
+      if (accesssToken) {
+        const parsedOuter = JSON.parse(accesssToken?.token);
+        const token: string = parsedOuter.token;
+        const response = await getAllClaims(token, page, limit);
+        setClaimsData(response);
+      }
+    } catch (error: any) {
+      showToast(error?.message, 'ERROR');
+    } finally {
+      setIsLoading(false);
     }
-}
+  };
 
-export default useClaimsScreen
+  useEffect(() => {
+    getClaimsData();
+  }, []);
+  return {
+    handleLogOutPress,
+    isLoading,
+    claimsData,
+  };
+};
+
+export default useClaimsScreen;
