d220c20c7a0d55161821900ff32fd5a0ecc58fa9
Ankit <devankitkakadiya@gmail.com>
2024-10-28 11:25:58 +0530

    Added back button

--- app/modules/ComponentScreens/save-money-view-screen/SaveMoneyViewScreen.tsx
+++ app/modules/ComponentScreens/save-money-view-screen/SaveMoneyViewScreen.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Alert, SafeAreaView } from 'react-native';
-import { SaveMoneyView } from '../../../components';
+
+import { StackNavigationProp } from '@react-navigation/stack';
+import { ParamListBase, useNavigation } from '@react-navigation/native';
+
+import { CustomBackButton, SaveMoneyView } from '../../../components';
 
 const SaveMoneyViewScreen = () => {
+  const navigation = useNavigation<StackNavigationProp<ParamListBase>>();
+
+  const handleBackPress = () => {
+    navigation.goBack();
+  };
+
   const handlePress = () => {
     Alert.alert('For Demo');
   };
+
   return (
     <SafeAreaView>
+      <CustomBackButton onBackPress={handleBackPress} />
       <SaveMoneyView
         heading="Save money on your medications"
         buttonTitle="Explore savings center"
