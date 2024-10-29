bc6d833546c09582e280fc493036e7783793b5db
Aniket Nimbark <devaniketnimbark@gmail.com>
2024-10-29 11:09:38 +0530

    Resolve font family issue | Added Back button in profile icon screen

--- app/modules/ComponentScreens/profile-icons-screen/ProfileIconsScreen.tsx
+++ app/modules/ComponentScreens/profile-icons-screen/ProfileIconsScreen.tsx
@@ -1,11 +1,20 @@
-import { FlatList, Text } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
+
+import { StackNavigationProp } from '@react-navigation/stack';
+import { ParamListBase, useNavigation } from '@react-navigation/native';
 
 import { profileIconsData } from '../../../utils';
 import { styles } from './ProfileIconsScreenStyles';
 import { profileIconsDataProps } from '../../../types';
-import { ProfileIconsItem, SafeAreaContainer } from '../../../components';
+import { CustomBackButton, ProfileIconsItem, SafeAreaContainer } from '../../../components';
 
 const ProfileIconsScreen = () => {
+  const navigation = useNavigation<StackNavigationProp<ParamListBase>>();
+
+  const handleBackPress = () => {
+    navigation.goBack();
+  };
+
   const renderItem = ({ item, index }: { item: profileIconsDataProps; index: number }) => {
     return (
       <ProfileIconsItem
@@ -17,16 +26,19 @@ const ProfileIconsScreen = () => {
     );
   };
   return (
-    <SafeAreaContainer style={styles.container}>
-      <Text style={styles.headingText}>{'Profile Icons Screen'}</Text>
-      <FlatList
-        horizontal
-        bounces={false}
-        data={profileIconsData}
-        renderItem={renderItem}
-        showsHorizontalScrollIndicator={false}
-        keyExtractor={item => item.id.toString()}
-      />
+    <SafeAreaContainer>
+      <CustomBackButton onBackPress={handleBackPress} />
+      <View style={styles.container}>
+        <Text style={styles.headingText}>{'Profile Icons Screen'}</Text>
+        <FlatList
+          horizontal
+          bounces={false}
+          data={profileIconsData}
+          renderItem={renderItem}
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={item => item.id.toString()}
+        />
+      </View>
     </SafeAreaContainer>
   );
 };
